a523583be39872caea93d54b594ec6f1b62b5780
Ok-Sushil <sushil26saini@gmail.com>
2025-02-24 12:27:39 +0530

    Day 1 java and leanred jdk, JRE, Basic Syntax,Hello world programme,all about  DataTypes

---
+++ DAY_1/BasicSyntax.java
@@ -0,0 +1,9 @@
+public class BasicSyntax{
+    public static void main(String[] args){
+        System.out.println("Hello Sushil");
+        int x=10;  // local variable
+        System.out.println(x);
+    }
+
+    // System.out.println(x); // not accessable 
+}
